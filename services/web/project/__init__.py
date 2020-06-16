aa3c8502bbe2e5c1322f6914f446a80389a3164e
carrenolg <carrenolg@hotmail.com>
2020-06-15 19:08:31 -0500

    flask-mongo-nginx mode production

--- services/web/project/__init__.py
+++ services/web/project/__init__.py
@@ -1,15 +1,34 @@
 from flask import Flask, jsonify, request
 from flask_cors import CORS
+from bson import ObjectId
+from flask_pymongo import PyMongo
 
 app = Flask(__name__)
 # Settings
 CORS(app)
+# Database set up
+app.config['MONGO_URI'] = 'mongodb://db:27017/dbprod'
+mongo = PyMongo(app)
+
+# Database
+db = mongo.db.dbprod
 
 
 @app.route('/', methods=['GET'])
 def home():
     data = {'hello': 'world'}
     return jsonify(data)
+
+
+# Routes
+@app.route('/user', methods=['POST'])
+def user():
+    id = db.insert({
+        "name": request.json['name'],
+        "email": request.json['email'],
+        "password": request.json['password']
+    })
+    return jsonify(str(ObjectId(id)))
 
 
 if __name__ == "__main__":
